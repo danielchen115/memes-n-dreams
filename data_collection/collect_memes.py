64860b6dc00b5fa48019d6595e482da698b12c87
Daniel <daniel.chen115@gmail.com>
2019-12-25 16:39:03 -0500

    set up praw

--- data_collection/collect_memes.py
+++ data_collection/collect_memes.py
@@ -5,4 +5,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-print(os.getenv("CLIENT_ID"))
+cwd = os.getcwd()
+
+reddit = praw.Reddit(
+        client_id=os.getenv("CLIENT_ID"),
+        client_secret=os.getenv("CLIENT_SECRET"),
+        username=os.getenv("USERNAME"),
+        password=os.getenv("PASSWORD"),
+        user_agent=os.getenv("USER_AGENT")
+        )
+
+memes = reddit.subreddit('memes').hot(limit=None)
+meme_urls = []
+for meme in memes:
+    meme_urls.append(meme.url)
+
+with open(cwd + "/../data_raw/meme_urls.txt", "w+") as f:
+    f.seek(0)
+    for url in meme_urls:
+        f.write(url + "\n")
+    f.truncate()
